a6394e2fd3e55a02e639ab2a6d6e666e45ee3a4e
jaytwwm <jaymehta262001@gmail.com>
2021-02-23 16:53:32 +0530

    Usability

--- popup.js
+++ popup.js
@@ -1,1 +1,20 @@
-// Future JavaScript will go here
+bug = document.getElementById("bug");
+
+function getResSt() {
+  bug_text = bug.value;
+  var uri = "https://stackoverflow.com/search?q=" + bug_text;
+  uri = encodeURI(uri);
+  openInNewTab(uri);
+}
+
+function openInNewTab(url) {
+  var win = window.open(url, "_blank");
+  win.focus();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var supremebot = document.getElementById("stov");
+  supremebot.addEventListener("click", function () {
+    getResSt();
+  });
+});
